13b9dfb1f21114a204bd8b14298a55920a9c8203
wangqi <120211Qq>
2020-05-19 11:18:29 +0800

    性能测试

--- performance/uat-space-id.py
+++ performance/uat-space-id.py
@@ -35,7 +35,7 @@ class test_task(TaskSet):
     @task()
     def test_upload(self):
         # 定义requests的请求头
-        header = {"Content-Type":"application/json","Access-Token":'RTAwRjAzRDhGMDdGMzk5RERFNUEzRjYzMjhFRjYxRUY1NzYwQkU3QzkwMjEzRjY5OTdDRUIzNTU1RDZCRUQwMQ=='}
+        header = {"Content-Type":"application/json","Access-Token":'MUQyMjM4QzFBMzIzODVENzRBOTE0NEVFOTk5QkU3RjJDMzM2MDAwQUYzQkI2RDEyQTcwODQ4QUQyMTEwQTAxOA=='}
         url = 'http://zhsq-iot-api.sunac.com.cn/v2/space-service/space/26f8dc9b6c32449a'
         body = {
 
